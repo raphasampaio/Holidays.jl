743bd450f9e346c5457f3b29523de2e426141b41
Documenter.jl <documenter@juliadocs.github.io>
2024-08-26 02:45:22 +0000

    build based on 2be28a7

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Holidays","page":"Home","title":"Holidays","text":"","category":"section"}]
+[{"location":"#Holidays","page":"Home","title":"Holidays","text":"","category":"section"},{"location":"#Overview","page":"Home","title":"Overview","text":"","category":"section"},{"location":"#Installation","page":"Home","title":"Installation","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"(v1.0) pkg> add Query","category":"page"},{"location":"#Highlights","page":"Home","title":"Highlights","text":"","category":"section"}]
 }
