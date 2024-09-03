076e75404935f7fa5f04f4571f74ec478dc2ec8e
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-09-03 00:16:24 -0300

    Project updates

--- test/calendars/islamic.jl
+++ test/calendars/islamic.jl
@@ -4,6 +4,7 @@ function test_islamic()
     @test Holidays.Islamic.is_eid_al_fitr_day_two(Date(2024, 4, 11)) == true
     @test Holidays.Islamic.is_eid_al_fitr_day_three(Date(2024, 4, 12)) == true
     @test Holidays.Islamic.is_eid_al_adha(Date(2024, 6, 16)) == true
+    @test Holidays.Islamic.is_mawlid(Date(2024, 9, 15)) == true
 
     # 1446 AH
     @test Holidays.Islamic.is_eid_al_fitr_day(Date(2025, 3, 30)) == true
