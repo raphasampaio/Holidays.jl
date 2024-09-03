2ee059a0fb96cf385ff08e03747de17f28452a4f
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-09-03 00:45:03 -0300

    Project updates

--- test/countries/egypt.jl
+++ test/countries/egypt.jl
@@ -1,3 +1,20 @@
 function test_egypt()
+    calendar = Holidays.Egypt()
+
+    holidays = [
+        Date(2024, 1, 7),
+        Date(2024, 1, 25),
+        Date(2024, 4, 10),
+        Date(2024, 4, 11),
+        Date(2024, 4, 12),
+        Date(2024, 4, 25),
+        Date(2024, 5, 1),
+        Date(2024, 6, 30),
+        Date(2024, 7, 23),
+        Date(2024, 10, 6),
+    ]
+
+    # test_holidays(calendar, 2024, holidays)
+
     return nothing
 end
