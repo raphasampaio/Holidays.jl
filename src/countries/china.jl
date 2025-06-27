93b968ade5a42933c7b5c1ea59823ade49593fd8
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-06-27 16:51:28 -0300

    Update

--- src/countries/china.jl
+++ src/countries/china.jl
@@ -11,26 +11,6 @@ const Chinese = Holidays.Chinese
 
 const China = Holidays.China
 
-function is_spring_festival(x::TimeType)
-    # Use the more accurate Chinese calendar implementation
-    return Chinese.is_chinese_new_year(x)
-end
-
-function is_tomb_sweeping_day(x::TimeType)
-    # Use the Chinese calendar for Qingming Festival
-    return Chinese.is_qingming_festival(x)
-end
-
-function is_dragon_boat_festival(x::TimeType)
-    # Use the Chinese calendar for Dragon Boat Festival
-    return Chinese.is_dragon_boat_festival(x)
-end
-
-function is_mid_autumn_festival(x::TimeType)
-    # Use the Chinese calendar for Mid-Autumn Festival
-    return Chinese.is_mid_autumn_festival(x)
-end
-
 function is_national_day(x::TimeType)
     return is_october(x) && Dates.day(x) == 1
 end
@@ -38,12 +18,12 @@ end
 function Holidays.fetch_holidays(::Type{China})
     return [
         Holiday("New Year's Day", Gregorian.is_new_years_day),
-        Holiday("Spring Festival", is_spring_festival),
+        Holiday("Spring Festival", Chinese.is_chinese_new_year),
         Holiday("Lantern Festival", Chinese.is_lantern_festival),
-        Holiday("Tomb-Sweeping Day", is_tomb_sweeping_day),
+        Holiday("Tomb-Sweeping Day", Chinese.is_qingming_festival),
         Holiday("Labour Day", International.is_workers_day),
-        Holiday("Dragon Boat Festival", is_dragon_boat_festival),
-        Holiday("Mid-Autumn Festival", is_mid_autumn_festival),
+        Holiday("Dragon Boat Festival", Chinese.is_dragon_boat_festival),
+        Holiday("Mid-Autumn Festival", Chinese.is_mid_autumn_festival),
         Holiday("National Day", is_national_day),
         # Note: Chinese holidays are now using proper lunar calendar calculations
         # from the Chinese calendar module for more accurate dates
