912bfdf83e0cd232674629de60ecd78c71863a39
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-13 19:58:43 -0300

    Update

--- src/countries/united_states.jl
+++ src/countries/united_states.jl
@@ -50,6 +50,10 @@ function is_juneteenth_national_independence_day(x::TimeType)
     return Dates.year(x) >= 2021 && is_june(x) && is_day(x, 19)
 end
 
+function is_patriot_day(x::TimeType)
+    return Dates.year(x) >= 1969 && is_april(x) && is_third_monday_of_month(x)
+end
+
 function Holidays.fetch_holidays(::Type{UnitedStates})
     return [
         Holiday("New Year's Day", is_new_years_day),
@@ -68,301 +72,322 @@ end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Alabama}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Alaska}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Seward's Day", x -> is_march(x) && is_last_monday_of_month(x)),
+        Holiday("Alaska Day", x -> is_october(x) && is_day(x, 18)),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Arizona}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Arkansas}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.California}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Cesar Chavez Day", x -> is_march(x) && is_day(x, 31)),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Colorado}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Cesar Chavez Day", x -> is_march(x) && is_day(x, 31)),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Connecticut}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Lincoln's Birthday", x -> is_february(x) && is_day(x, 12)),
+        Holiday("Good Friday", Christian.is_good_friday),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Delaware}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Florida}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Georgia}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Hawaii}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Prince Jonah Kuhio Kalanianaole Day", x -> is_march(x) && is_day(x, 26)),
+        Holiday("King Kamehameha I Day", x -> is_june(x) && is_day(x, 11)),
+        Holiday("Statehood Day", x -> is_august(x) && is_third_friday_of_month(x)),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Idaho}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Illinois}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Lincoln's Birthday", x -> is_february(x) && is_day(x, 12)),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Indiana}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Good Friday", Christian.is_good_friday),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Iowa}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Kansas}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Kentucky}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Good Friday", Christian.is_good_friday),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Louisiana}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Mardi Gras", Christian.is_shrove_tuesday),
+        Holiday("Good Friday", Christian.is_good_friday),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Maine}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Patriot's Day", is_patriot_day),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Maryland}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Massachusetts}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Patriot's Day", is_patriot_day),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Michigan}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Minnesota}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Mississippi}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Mardi Gras", Christian.is_shrove_tuesday),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Missouri}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Lincoln's Birthday", x -> is_february(x) && is_day(x, 12)),
+        Holiday("Truman Day", x -> is_may(x) && is_day(x, 8)),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Montana}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Nebraska}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Arbor Day", x -> is_april(x) && is_last_friday_of_month(x)),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Nevada}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
+        Holiday("Family Day", x -> is_november(x) && is_day(x, 29)),
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.NewHampshire}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.NewJersey}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.NewMexico}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.NewYork}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.NorthCarolina}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.NorthDakota}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Ohio}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Oklahoma}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Oregon}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Pennsylvania}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.RhodeIsland}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.SouthCarolina}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.SouthDakota}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Tennessee}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Texas}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Utah}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Vermont}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Virginia}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Washington}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.WestVirginia}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Wisconsin}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
 function Holidays.fetch_holidays(::Type{UnitedStates{Subdivision.Wyoming}})
     return [
-        Holidays.fetch_holidays(UnitedStates)...,
+        fetch_holidays(UnitedStates)...,
     ]
 end
 
--- test/countries/test_united_states.jl
+++ test/countries/test_united_states.jl
@@ -9,7 +9,6 @@ include("../util.jl")
 @testset "UnitedStates" begin
     calendar = Holidays.UnitedStates()
 
-    # Federal holidays for the United States in 2024
     holidays = [
         Date(2024, 1, 1),  # New Year's Day
         Date(2024, 1, 15), # Martin Luther King Jr. Day
@@ -26,51 +25,147 @@ include("../util.jl")
 
     test_holidays(calendar, 2024, holidays)
 
-    # @testset "Alaska" begin
-    #     calendar = Holidays.UnitedStates(subdivision = Subdivision.Alaska())
-    #     test_holidays(calendar, 2024, holidays, [Date(2024, 10, 18)]) # Alaska Day
-    # end
+    @testset "Alabama" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Alabama())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
 
-    # @testset "California" begin
-    #     calendar = Holidays.UnitedStates(subdivision = Subdivision.California())
-    #     test_holidays(calendar, 2024, holidays, [Date(2024, 3, 31)])  # César Chávez Day
-    # end
+    @testset "Alaska" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Alaska())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 3, 25), Date(2024, 10, 18)])
+    end
 
-    # @testset "Florida" begin
-    #     calendar = Holidays.UnitedStates(subdivision = Subdivision.Florida())
-    #     test_holidays(calendar, 2024, holidays, [Date(2024, 4, 2)])  # Pascua Florida Day
-    # end
+    @testset "Arizona" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Arizona())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
 
-    # @testset "Hawaii" begin
-    #     calendar = Holidays.UnitedStates(subdivision = Subdivision.Hawaii())
-    #     test_holidays(calendar, 2024, holidays, [Date(2024, 3, 26)])  # Prince Kūhiō Day
-    # end
+    @testset "Arkansas" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Arkansas())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
 
-    # @testset "Illinois" begin
-    #     calendar = Holidays.UnitedStates(subdivision = Subdivision.Illinois())
-    #     test_holidays(calendar, 2024, holidays, [Date(2024, 2, 12)])  # Lincoln's Birthday
-    # end
+    @testset "California" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.California())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 3, 31)])
+    end
 
-    # @testset "Louisiana" begin
-    #     calendar = Holidays.UnitedStates(subdivision = Subdivision.Louisiana())
-    #     test_holidays(calendar, 2024, holidays, [Date(2024, 2, 13)])  # Mardi Gras
-    # end
+    @testset "Colorado" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Colorado())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 3, 31)])
+    end
 
-    # @testset "Massachusetts" begin
-    #     calendar = Holidays.UnitedStates(subdivision = Subdivision.Massachusetts())
-    #     test_holidays(calendar, 2024, holidays, [Date(2024, 4, 15)])  # Patriots' Day
-    # end
+    @testset "Connecticut" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Connecticut())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 2, 12), Date(2024, 3, 29)])
+    end
 
-    # @testset "NewYork" begin
-    #     calendar = Holidays.UnitedStates(subdivision = Subdivision.NewYork())
-    #     test_holidays(calendar, 2024, holidays, [Date(2024, 2, 12)])  # Lincoln's Birthday
-    # end
+    @testset "Delaware" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Delaware())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
 
-    # @testset "Texas" begin
-    #     calendar = Holidays.UnitedStates(subdivision = Subdivision.Texas())
-    #     test_holidays(calendar, 2024, holidays, [Date(2024, 3, 2), Date(2024, 6, 19)]) # Texas Independence Day, Emancipation Day in Texas
-    # end
+    @testset "Florida" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Florida())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
+
+    @testset "Georgia" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Georgia())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
+
+    @testset "Hawaii" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Hawaii())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 3, 26), Date(2024, 6, 11), Date(2024, 8, 16)])
+    end
+
+    @testset "Idaho" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Idaho())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
+
+    @testset "Illinois" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Illinois())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 2, 12)])
+    end
+
+    @testset "Indiana" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Indiana())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 3, 29)])
+    end
+
+    @testset "Iowa" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Iowa())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
+
+    @testset "Kansas" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Kansas())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
+
+    @testset "Kentucky" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Kentucky())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 3, 29)])
+    end
+
+    @testset "Louisiana" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Louisiana())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 2, 13), Date(2024, 3, 29)])
+    end
+
+    @testset "Maine" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Maine())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 4, 15)])
+    end
+
+    @testset "Maryland" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Maryland())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
+
+    @testset "Massachusetts" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Massachusetts())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 4, 15)])
+    end
+
+    @testset "Michigan" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Michigan())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
+
+    @testset "Minnesota" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Minnesota())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
+
+    @testset "Mississippi" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Mississippi())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 2, 13)])
+    end
+
+    @testset "Missouri" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Missouri())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 2, 12), Date(2024, 5, 8)])
+    end
+
+    @testset "Montana" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Montana())
+        test_holidays(calendar, 2024, holidays, Date[])
+    end
+
+    @testset "Nebraska" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Nebraska())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 4, 26)])
+    end
+
+    @testset "Nevada" begin
+        calendar = Holidays.UnitedStates(subdivision = Subdivision.Nevada())
+        test_holidays(calendar, 2024, holidays, [Date(2024, 11, 29)])
+    end
 
     return nothing
 end
+
 end
