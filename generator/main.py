11407c9ded6c0f41d9f671b774bbfb0b8526674a
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-20 22:46:16 -0300

    Update

--- generator/main.py
+++ generator/main.py
@@ -1,12 +1,13 @@
 import sys
+from datetime import date
+
 import holidays
-from datetime import date, timedelta
 
 country = sys.argv[1]
 start_year = int(sys.argv[2])
 end_year = int(sys.argv[3])
 
 print("    holidays = [")
-for date, name in sorted(holidays.country_holidays(country, years=range(start_year, end_year+1)).items()):
+for date, name in sorted(holidays.country_holidays(country, years=range(start_year, end_year + 1)).items()):
     print(f"        Date({date.year}, {date.month}, {date.day}), # {name}")
 print("    ]")
