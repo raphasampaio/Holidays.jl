d8547664438f715a769dd0e5b7c136e2a93ae1a4
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-21 23:40:52 -0300

    Update

--- src/countries/norway.jl
+++ src/countries/norway.jl
@@ -20,13 +20,13 @@ function Holidays.fetch_holidays(::Type{Holidays.Norway})
         Holiday("Good Friday", Christian.is_good_friday),
         Holiday("Easter Sunday", Christian.is_easter),
         Holiday("Easter Monday", Christian.is_easter_monday),
-        Holiday("Labour Day", International.is_workers_day),
+        Holiday("Labor Day", International.is_workers_day),
         Holiday("Constitution Day", is_constitution_day),
         Holiday("Ascension Day", Christian.is_ascension_day),
         Holiday("Whit Sunday", Christian.is_whit_sunday),
         Holiday("Whit Monday", Christian.is_whit_monday),
         Holiday("Christmas Day", Christian.is_christmas_day),
-        Holiday("Boxing Day", Christian.is_boxing_day),
+        Holiday("Second Day of Christmas", Christian.is_boxing_day),
     ]
 end
 
