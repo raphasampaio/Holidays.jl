d310df68a44d31c95d2169d8827b888f1c4b1719
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 16:50:36 -0300

    Update

--- test/countries/test_kenya.jl
+++ test/countries/test_kenya.jl
@@ -9,7 +9,7 @@ include("../util.jl")
 @testset "Kenya" begin
     calendar = Holidays.Kenya()
 
-    holidays = [
+    holidays = Date[
         Date(1970, 1, 1), # New Year's Day
         Date(1971, 1, 1), # New Year's Day
         Date(1972, 1, 1), # New Year's Day
