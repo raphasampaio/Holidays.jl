9a214517c17f5ec06e437bef592f0e88cfdd0255
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 12:40:44 -0300

    Update

---
+++ src/countries/aruba.jl
@@ -0,0 +1,16 @@
+module ArubaHolidays
+
+using Dates
+using Holidays
+
+include("../dates.jl")
+
+const Gregorian = Holidays.Gregorian
+
+function Holidays.fetch_holidays(::Type{Holidays.Aruba})
+    return [
+        Holiday("New Year's Day", Gregorian.is_new_years_day),
+    ]
+end
+
+end
