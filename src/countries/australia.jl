f8d0710b657b768523299be7a7dc675ac264f042
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 17:24:37 -0300

    Update

--- src/countries/australia.jl
+++ src/countries/australia.jl
@@ -27,15 +27,15 @@ end
 function Holidays.fetch_holidays(::Type{Holidays.Holidays.Australia})
     return [
         Holiday("New Year's Day", International.is_new_years_day),
-        Holiday("Australia Day", is_australia_day),
-        Holiday("Good Friday", Christian.is_good_friday),
-        Holiday("Easter Saturday", Christian.is_holy_saturday),
-        Holiday("Easter Monday", Christian.is_easter_monday),
-        Holiday("ANZAC Day", is_anzac_day),
-        Holiday("Queen's Birthday", is_queens_birthday),
-        Holiday("Labour Day", is_labour_day_october),
-        Holiday("Christmas Day", Christian.is_christmas_day),
-        Holiday("Boxing Day", Christian.is_boxing_day),
+        # Holiday("Australia Day", is_australia_day),
+        # Holiday("Good Friday", Christian.is_good_friday),
+        # Holiday("Easter Saturday", Christian.is_holy_saturday),
+        # Holiday("Easter Monday", Christian.is_easter_monday),
+        # Holiday("ANZAC Day", is_anzac_day),
+        # Holiday("Queen's Birthday", is_queens_birthday),
+        # Holiday("Labour Day", is_labour_day_october),
+        # Holiday("Christmas Day", Christian.is_christmas_day),
+        # Holiday("Boxing Day", Christian.is_boxing_day),
     ]
 end
 
--- src/countries/austria.jl
+++ src/countries/austria.jl
@@ -23,18 +23,18 @@ end
 function Holidays.fetch_holidays(::Type{Holidays.Austria})
     return [
         Holiday("New Year's Day", International.is_new_years_day),
-        Holiday("Epiphany", is_epiphany),
-        Holiday("Easter Monday", Christian.is_easter_monday),
-        Holiday("Labour Day", International.is_workers_day),
-        Holiday("Ascension Day", Christian.is_ascension_day),
-        Holiday("Whit Monday", Christian.is_whit_monday),
-        Holiday("Corpus Christi", Christian.is_corpus_christi),
-        Holiday("Assumption Day", Christian.is_assumption_day),
-        Holiday("National Day", is_national_day),
-        Holiday("All Saints' Day", Christian.is_all_saints_day),
-        Holiday("Immaculate Conception", is_immaculate_conception),
-        Holiday("Christmas Day", Christian.is_christmas_day),
-        Holiday("Boxing Day", Christian.is_boxing_day),
+        # Holiday("Epiphany", is_epiphany),
+        # Holiday("Easter Monday", Christian.is_easter_monday),
+        # Holiday("Labour Day", International.is_workers_day),
+        # Holiday("Ascension Day", Christian.is_ascension_day),
+        # Holiday("Whit Monday", Christian.is_whit_monday),
+        # Holiday("Corpus Christi", Christian.is_corpus_christi),
+        # Holiday("Assumption Day", Christian.is_assumption_day),
+        # Holiday("National Day", is_national_day),
+        # Holiday("All Saints' Day", Christian.is_all_saints_day),
+        # Holiday("Immaculate Conception", is_immaculate_conception),
+        # Holiday("Christmas Day", Christian.is_christmas_day),
+        # Holiday("Boxing Day", Christian.is_boxing_day),
     ]
 end
 
