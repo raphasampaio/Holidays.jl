c41c5cd647b1450698f75ab73bb2384c37841a09
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 15:59:15 -0300

    Update

--- src/countries/cook_islands.jl
+++ src/countries/cook_islands.jl
@@ -6,7 +6,9 @@ using Holidays
 include("../dates.jl")
 
 function Holidays.fetch_holidays(::Type{Holidays.CookIslands})
-    return []
+    return [
+        Holiday("New Year's Day", x -> Dates.year(x) >= 2000 && Gregorian.is_new_years_day(x)),
+    ]
 end
 
 end
