4ebc995221b008d2d7c8a50e21011ae2a9da87db
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 17:02:49 -0300

    Update

--- src/countries/armenia.jl
+++ src/countries/armenia.jl
@@ -7,10 +7,21 @@ include("../dates.jl")
 
 const Gregorian = Holidays.Gregorian
 
+function is_new_years_day(x::TimeType)
+    if Dates.year(x) >= 1991 && is_january(x) && (is_day(x, 1) || is_day(x, 2))
+        return true
+    end
+
+    if is_year_between(x, 2010, 2021) && is_january(x) && (is_day(x, 3) || is_day(x, 4))
+        return true
+    end
+
+    return false
+end
+
 function Holidays.fetch_holidays(::Type{Holidays.Armenia})
     return [
-        Holiday("New Year's Day", x -> Dates.year(x) >= 1991 && Gregorian.is_new_years_day(x)),
-        Holiday("New Year's Day", x -> Dates.year(x) >= 1991 && is_january(x) && is_day(x, 2)),
+        Holiday("New Year's Day", is_new_years_day),
     ]
 end
 
