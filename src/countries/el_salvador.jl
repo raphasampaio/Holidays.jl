fe71ffa0dd704d877e8a3bae4492faa5d7668ca0
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 10:51:47 -0300

    Update

--- src/countries/el_salvador.jl
+++ src/countries/el_salvador.jl
@@ -5,8 +5,12 @@ using Holidays
 
 include("../dates.jl")
 
+const Gregorian = Holidays.Gregorian
+
 function Holidays.fetch_holidays(::Type{Holidays.ElSalvador})
-    return []
+    return [
+        Holiday("New Year's Day", x -> Dates.year(x) >= 1973 && Gregorian.is_new_years_day(x)),
+    ]
 end
 
 end
--- test/countries/test_slovakia.jl
+++ test/countries/test_slovakia.jl
@@ -9,7 +9,46 @@ include("../util.jl")
 @testset "Slovakia" begin
     calendar = Holidays.Slovakia()
 
-    holidays = Date[]
+    holidays = [
+        Date(1993, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(1994, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(1995, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(1996, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(1997, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(1998, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(1999, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2000, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2001, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2002, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2003, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2004, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2005, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2006, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2007, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2008, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2009, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2010, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2011, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2012, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2013, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2014, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2015, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2016, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2017, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2018, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2019, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2020, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2021, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2022, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2023, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2024, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2025, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2026, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2027, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2028, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2029, 1, 1), # Day of the Establishment of the Slovak Republic
+        Date(2030, 1, 1), # Day of the Establishment of the Slovak Republic
+    ]
 
     test_holidays(calendar, holidays)
 
