4b385f89c8d40e3557fcf5d9b077dba8eb5d71f1
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 16:51:11 -0300

    Update

--- src/countries/laos.jl
+++ src/countries/laos.jl
@@ -5,8 +5,12 @@ using Holidays
 
 include("../dates.jl")
 
+const Gregorian = Holidays.Gregorian
+
 function Holidays.fetch_holidays(::Type{Holidays.Laos})
-    return []
+    return [
+        Holiday("New Year's Day", x -> Dates.year(x) >= 1976 && Gregorian.is_new_years_day(x)),
+    ]
 end
 
 end
