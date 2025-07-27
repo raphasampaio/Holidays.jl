6bbebd03c482bedfc2525503bcb007f072c6fca4
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 12:40:35 -0300

    Update

---
+++ src/countries/greenland.jl
@@ -0,0 +1,16 @@
+module GreenlandHolidays
+
+using Dates
+using Holidays
+
+include("../dates.jl")
+
+const Gregorian = Holidays.Gregorian
+
+function Holidays.fetch_holidays(::Type{Holidays.Greenland})
+    return [
+        Holiday("New Year's Day", x -> Dates.year(x) >= 1979 && Gregorian.is_new_years_day(x)),
+    ]
+end
+
+end
