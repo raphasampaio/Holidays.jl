c07e16ef2136bd3fc24770cbdfcdf301a66daf48
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 15:18:42 -0300

    Update

---
+++ src/countries/new_caledonia.jl
@@ -0,0 +1,12 @@
+module NewCaledoniaHolidays
+
+using Dates
+using Holidays
+
+include("../dates.jl")
+
+function Holidays.fetch_holidays(::Type{Holidays.NewCaledonia})
+    return Date[]
+end
+
+end
