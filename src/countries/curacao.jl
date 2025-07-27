2cf06b1ac48eb5aaa856aba4bbd7247deacafdcb
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 16:02:45 -0300

    Update

--- src/countries/curacao.jl
+++ src/countries/curacao.jl
@@ -5,8 +5,13 @@ using Holidays
 
 include("../dates.jl")
 
+const Gregorian = Holidays.Gregorian
+
 function Holidays.fetch_holidays(::Type{Holidays.Curacao})
-    return []
+    return [
+        Holiday("New Year's Day", Gregorian.is_new_years_day),
+    ]
+    
 end
 
 end
