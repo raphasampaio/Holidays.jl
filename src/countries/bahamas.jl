298eb989aaaa1750d6fda89596d756de78ce3b47
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 12:45:01 -0300

    Update

--- src/countries/bahamas.jl
+++ src/countries/bahamas.jl
@@ -5,8 +5,12 @@ using Holidays
 
 include("../dates.jl")
 
+const Gregorian = Holidays.Gregorian
+
 function Holidays.fetch_holidays(::Type{Holidays.Bahamas})
-    return []
+    return [
+        Holiday("New Year's Day", x -> Dates.year(x) >= 1974 && Gregorian.is_new_years_day(x)),
+    ]
 end
 
 end
