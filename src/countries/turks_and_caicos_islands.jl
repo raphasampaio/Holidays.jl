72fcbd26f2faa5984fd6ab803b59d110f8fe8a67
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 15:30:18 -0300

    Update

---
+++ src/countries/turks_and_caicos_islands.jl
@@ -0,0 +1,12 @@
+module TurksAndCaicosIslandsHolidays
+
+using Dates
+using Holidays
+
+include("../dates.jl")
+
+function Holidays.fetch_holidays(::Type{Holidays.TurksAndCaicosIslands})
+    return Date[]
+end
+
+end
