cb4bffa26a23769a21f9b5333ddb64a9738b6d86
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 16:22:34 -0300

    Update

--- src/countries/saint_martin.jl
+++ src/countries/saint_martin.jl
@@ -5,8 +5,12 @@ using Holidays
 
 include("../dates.jl")
 
+const Gregorian = Holidays.Gregorian
+
 function Holidays.fetch_holidays(::Type{Holidays.SaintMartin})
-    return []
+    return [
+        Holiday("New Year's Day", x -> Dates.year(x) >= 2008 && Gregorian.is_new_years_day(x)),
+    ]
 end
 
 end
