d5e0a5815693ba4a809edcb0d66761c06569eead
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-07-27 00:22:51 -0300

    Update

--- src/countries/estonia.jl
+++ src/countries/estonia.jl
@@ -6,7 +6,9 @@ using Holidays
 include("../dates.jl")
 
 function Holidays.fetch_holidays(::Type{Holidays.Estonia})
-    return []
+    return [
+        Holiday("New Year's Day", Gregorian.is_new_years_day),
+    ]
 end
 
 end
