c701de51076016301a3c57b7097797bfd0a2dbf1
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-08-06 22:20:34 -0300

    Update

--- src/countries/sweden.jl
+++ src/countries/sweden.jl
@@ -7,9 +7,16 @@ include("../dates.jl")
 
 const Christian = Holidays.Christian
 
+function is_new_years_day(x::TimeType)
+    if is_sunday(x)
+        return false
+    end
+    return is_january_1st(x)
+end
+
 function Holidays.fetch_holidays(::Type{Holidays.Sweden})
     return [
-        Holiday("New Year's Day", is_january_1st),
+        Holiday("New Year's Day", is_new_years_day),
     ]
 end
 
