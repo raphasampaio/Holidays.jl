a7d3601c469a28a3458ac8145974e4430e4f2b8c
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-09-21 16:07:07 -0300

    Update

--- src/countries/belgium.jl
+++ src/countries/belgium.jl
@@ -7,9 +7,22 @@ include("../dates.jl")
 
 const Christian = Holidays.Christian
 
-function Holidays.fetch_holidays(::Type{Holidays.Belgium})
+const Belgium = Holidays.Belgium
+
+function Holidays.fetch_holidays(::Type{Belgium})
     return [
         Holiday("New Year's Day", is_january_1st),
+        Holiday("Easter Sunday", Christian.is_easter_sunday),
+        Holiday("Easter Monday", Christian.is_easter_monday),
+        Holiday("Labor Day", is_may_1st),
+        Holiday("Ascension Day", Christian.is_ascension_day),
+        Holiday("Whit Sunday", Christian.is_whit_sunday),
+        Holiday("Whit Monday", Christian.is_whit_monday),
+        Holiday("National Day", is_july_21st),
+        Holiday("Assumption Day", is_august_15th),
+        Holiday("All Saints' Day", is_november_1st),
+        Holiday("Armistice Day", is_november_11th),
+        Holiday("Christmas Day", Christian.is_christmas_day),
     ]
 end
 
