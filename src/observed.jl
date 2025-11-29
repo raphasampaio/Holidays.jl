9c7964d30c556a5c2532120f379bc2ce58c63bb5
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-11-29 00:46:12 -0300

    Update

--- src/observed.jl
+++ src/observed.jl
@@ -23,3 +23,18 @@ function next_monday_if_falls_on_sunday(holiday::Holiday, x::TimeType)
     end
     return false
 end
+
+# Tuesday/Wednesday -> previous Monday, Thursday/Friday -> next Monday
+function previous_monday_if_falls_on_tuesday_or_wednesday_or_next_monday_if_falls_on_thursday_or_friday(holiday::Holiday, x::TimeType)
+    # Only applies if x is a Monday
+    is_monday(x) || return false
+
+    # If holiday falls on Tue/Wed -> moves to previous Monday (this x)
+    # If holiday falls on Thu/Fri -> moves to next Monday (this x)
+    return (
+        holiday.handler(x + Day(1)) ||  # Tuesday (next day)
+        holiday.handler(x + Day(2)) ||  # Wednesday (2 days after)
+        holiday.handler(x - Day(4)) ||  # Thursday (4 days before)
+        holiday.handler(x - Day(3))     # Friday (3 days before)
+    )
+end
